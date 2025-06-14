4cbb2c80ed2e6bd4f23bdf04c479594d5e6aa6af
Observatorio KAI <observatoriolgbtikaihn@gmail.com>
2025-06-14 00:49:28 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Puedes quitar esto si no usas Tailwind u otro CSS
-import App from './App';
+import App from './App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
