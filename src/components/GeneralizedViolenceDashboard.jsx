a021c0b2d560db4e67473876c95c84a0db5fdfa3
Observatorio KAI <observatoriolgbtikaihn@gmail.com>
2025-06-14 01:08:51 -0600

    Update GeneralizedViolenceDashboard.jsx

--- src/components/GeneralizedViolenceDashboard.jsx
+++ src/components/GeneralizedViolenceDashboard.jsx
@@ -3,6 +3,7 @@ import useGoogleSheetData from "../hooks/useGoogleSheetData";
 import DashboardCard from "./DashboardCard";
 import BarChartComponent from "./BarChartComponent";
 import TableComponent from "./TableComponent";
+import FilterContext from "../context/FilterContext";
 
 const GeneralizedViolenceDashboard = () => {
   const { data, loading, error } = useGoogleSheetData("Violencia Generalizada");
