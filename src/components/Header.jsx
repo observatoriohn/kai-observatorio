3bdcd4df41fc846959c619e41d9b1d116fd904df
Observatorio KAI <observatoriolgbtikaihn@gmail.com>
2025-06-14 02:00:32 -0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = ({ setActiveTab }) => {
   return (
     <header className="bg-gradient-to-r from-pink-600 via-purple-600 to-indigo-600 text-white p-6 flex justify-between items-center shadow-md">
-      <img src="/Logo KAI 2025.png" alt="KAI+ Logo" className="h-16" />
+      <img src="/logo-kai-2025.png" alt="KAI+ Logo" className="h-16" />
       <nav>
         <ul className="flex space-x-6 font-medium">
           <li><button onClick={() => setActiveTab("pillars")} className="hover:underline">Pilares</button></li>
