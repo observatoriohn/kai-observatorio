986f77cf65b84e17f253bbb0ab1e11464d6e9454
Observatorio KAI <observatoriolgbtikaihn@gmail.com>
2025-06-14 01:14:34 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Header from "./components/Header";
+//import Header from "./components/Header";
 import PillarNavigation from "./components/PillarNavigation";
 import ViolentDeathsDashboard from "./components/ViolentDeathsDashboard";
 import GeneralizedViolenceDashboard from "./components/GeneralizedViolenceDashboard";
